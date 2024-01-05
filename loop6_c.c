fb53518c7c7b6b50775dc229ddf6945688a9042a
meetkakadiya <meetkakadiya308@gmail.com>
2024-01-05 18:11:55 +0530

    loop_2.c.

---
+++ loop6_c.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+void main(){
+	
+	int i,j,k=1;
+    char a= 'A';
+	      int=j;      
+	for(i=1;i<=5;i++){
+		for(j=1;j<=5;j++){
+			
+			if(j%2!=0){
+				printf("%c", a++);
+			}else{
+				
+				printf("%d",k);
+				k++;
+			}
+			 
+		}
+		  printf("\n");
+}
+}
+                 	
+			
+	
